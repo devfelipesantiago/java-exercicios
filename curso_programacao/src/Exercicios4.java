9803f66d997654c0d47eef1429bd8cae56b3b8db
felipe <felipesantiagolds@gmail.com>
2021-08-22 11:37:19 -0300

    Estrutura de repeticao

--- curso_programacao/src/Exercicios4.java
+++ curso_programacao/src/Exercicios4.java
@@ -53,6 +53,54 @@ public class Exercicios4 {
         System.out.println("Gasolina: " + gasolina);
         System.out.println("Diesel: " + diesel);
 
+        int x = sc.nextInt();
+
+        for (int i = 1; i <= x; i++) {
+            if (i % 2 != 0) {
+                System.out.println(i);
+            }
+        }
+
+        int n = sc.nextInt();
+
+        int in = 0;
+        int out = 0;
+
+        for (int i = 0; i < n; i++) {
+            int x = sc.nextInt();
+            if (x >= 10 && x <= 20) {
+                in = in + 1;
+            } else {
+                out = out + 1;
+            }
+        }
+
+        System.out.println(in + " in");
+        System.out.println(out + " out");
+
+        int num = sc.nextInt();
+
+        for (int i = 0; i < num; i++) {
+
+            double a = sc.nextDouble();
+            double b = sc.nextDouble();
+            double c = sc.nextDouble();
+
+            double media = (a * 2.0 + b * 3.0 + c * 5.0) / 10.0;
+
+            System.out.printf("%.1f%n", media);
+        }
+
+        int b = sc.nextInt();
+
+        for (int i = 1; i <= b; i++) {
+
+            int primeiro = i;
+            int segundo = i * i;
+            int terceiro = i * i * i;
+            System.out.printf("%d %d %d%n", primeiro, segundo, terceiro);
+        }
+
         sc.close();
     }
 }
