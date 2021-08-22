aebd2b9f0d2ce28c83e5b1b4815e0c57e45c13d5
felipe <felipesantiagolds@gmail.com>
2021-08-21 21:00:56 -0300

    Funcoes matematicas

---
+++ curso_programacao/src/Exercises2.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Exercises2 {
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String x = sc.next();
+        int b = sc.nextInt();
+
+        System.out.println("Escreva o x: " + x);
+        System.out.println("Escreva o b: " + b);
+
+        sc.close();
+    }
+}
+
