16390d435fb4cf4c681ff49fa49be9c11683c9c9
felipe <felipesantiagolds@gmail.com>
2021-08-28 20:04:32 -0300

    App rent room

---
+++ curso_programacao/src/course/POO/AppRent.java
@@ -0,0 +1,38 @@
+package course.POO;
+
+import course.entities.Rent;
+
+import java.util.Scanner;
+
+public class AppRent {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        Rent[] room = new Rent[10];
+        System.out.print("How many rooms will be rented? ");
+        int quantityRoom = sc.nextInt();
+
+        for (int i = 1; i <= quantityRoom; i++) {
+            System.out.println("Rent #"+i+":");
+            System.out.print("Name: ");
+            sc.nextLine();
+            String name = sc.nextLine();
+            System.out.print("Email: ");
+            String email = sc.nextLine();
+            System.out.println("Room: ");
+            int numberRoom = sc.nextInt();
+            room[numberRoom] = new Rent(name, email);
+        }
+
+        System.out.println("Busy rooms:");
+        for (int i = 0; i < room.length; i++) {
+            if (room[i] != null){
+                System.out.println(i+": "+room[i]);
+            }
+        }
+
+        sc.close();
+    }
+}
