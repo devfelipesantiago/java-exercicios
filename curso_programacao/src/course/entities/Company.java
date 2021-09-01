7a448fb8a9318fdf5517d7687d03d71753f8d324
felipe <felipesantiagolds@gmail.com>
2021-09-01 09:54:16 -0300

    App tax payer

---
+++ curso_programacao/src/course/entities/Company.java
@@ -0,0 +1,29 @@
+package course.entities;
+
+public class Company extends TaxPayer{
+
+    private Integer numberOfEmployees;
+
+    public Company(String name, Double annualIncome, Integer numberOfEmployees) {
+        super(name, annualIncome);
+        this.numberOfEmployees = numberOfEmployees;
+    }
+
+    public Integer getNumberOfEmployee() {
+        return numberOfEmployees;
+    }
+
+    public void setNumberOfEmployee(Integer numberOfEmployee) {
+        this.numberOfEmployees = numberOfEmployee;
+    }
+
+    @Override
+    public double tax() {
+        if (numberOfEmployees > 10) {
+            return getAnnualIncome() * 0.14;
+        }
+        else {
+            return getAnnualIncome() * 0.16;
+        }
+    }
+}
