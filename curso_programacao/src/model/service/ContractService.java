6775232ec24076ac413f08205908b6ff9c80e418
felipe <felipesantiagolds@gmail.com>
2021-09-04 12:50:13 -0300

    Exercises with interface

---
+++ curso_programacao/src/model/service/ContractService.java
@@ -0,0 +1,13 @@
+package model.service;
+
+import model.entities.Contract;
+
+public class ContractService {
+
+    public ContractService(PaypalService paypalService) {
+
+    }
+
+    public void processContract(Contract contract, int n) {
+    }
+}
