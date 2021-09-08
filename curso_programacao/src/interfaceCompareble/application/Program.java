8df2d50a03886afffecb65879f6a8268448fefa4
felipe <felipesantiagolds@gmail.com>
2021-09-08 17:40:30 -0300

    Interface Compareble

--- curso_programacao/src/interfaceCompareble/application/Program.java
+++ curso_programacao/src/interfaceCompareble/application/Program.java
@@ -1,5 +1,7 @@
 package interfaceCompareble.application;
 
+import interfaceCompareble.entities.Employee;
+
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
@@ -10,17 +12,18 @@ import java.util.List;
 public class Program {
 
     public static void main(String[] args) {
-        List<String> list = new ArrayList<>();
+        List<Employee> list = new ArrayList<>();
         String path = "C:\\temp\\in.txt";
         try (BufferedReader br = new BufferedReader(new FileReader(path))) {
-            String name = br.readLine();
-            while (name != null) {
-                list.add(name);
-                name = br.readLine();
+            String employeeCsv = br.readLine();
+            while (employeeCsv != null) {
+                String[] fields = employeeCsv.split(",");
+                list.add(new Employee(fields[0], Double.parseDouble(fields[1])));
+                employeeCsv = br.readLine();
             }
             Collections.sort(list);
-            for (String s : list) {
-                System.out.println(s);
+            for (Employee emp : list) {
+                System.out.println(emp.getName() + ", " + emp.getSalary());
             }
         } catch (IOException e) {
             System.out.println("Error: " + e.getMessage());
