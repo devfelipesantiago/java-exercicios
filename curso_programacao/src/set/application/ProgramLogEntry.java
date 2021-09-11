717a8d835fd5f78325f235fb7be47692193d3ab2
felipe <felipesantiagolds@gmail.com>
2021-09-11 10:26:12 -0300

    Set hashSet

---
+++ curso_programacao/src/set/application/ProgramLogEntry.java
@@ -0,0 +1,50 @@
+package set.application;
+
+import set.entities.LogEntry;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.time.Instant;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class ProgramLogEntry {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter file full path: ");
+        String path = sc.nextLine();
+
+        // ler o arquivo
+        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+
+            Set<LogEntry> set = new HashSet<>();
+            // ler linha por linha do arquivo
+            String line = br.readLine();
+            while (line != null) {
+
+                String[] fields = line.split(" ");
+                String username = fields[0];
+                Date moment = Date.from(Instant.parse(fields[1]));
+
+                set.add(new LogEntry(username, moment));
+
+                line = br.readLine();
+            }
+            System.out.println("Total users: " + set.size());
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            System.out.println("Error: "+ e.getMessage());;
+        }
+
+        sc.close();
+    }
+}
--- curso_programacao/src/set/entities/Product.java
+++ curso_programacao/src/set/entities/Product.java
@@ -11,12 +11,14 @@ public class Product {
     public String getName() {
         return name;
     }
+
     public void setName(String name) {
         this.name = name;
     }
     public Double getPrice() {
         return price;
     }
+
     public void setPrice(Double price) {
         this.price = price;
     }
