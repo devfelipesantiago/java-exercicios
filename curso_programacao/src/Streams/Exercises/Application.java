18150858db86b5dba5d47b1ad3072df8f081b271
felipe <felipesantiagolds@gmail.com>
2021-09-11 16:54:57 -0300

    Exercise to reforce

---
+++ curso_programacao/src/Streams/Exercises/Application.java
@@ -0,0 +1,54 @@
+package Streams.Exercises;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class Application {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter full file path: ");
+        String path = sc.nextLine();
+
+        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+
+            List<Employee> list = new ArrayList<>();
+
+            String line = br.readLine();
+            while (line != null) {
+                String[] fields = line.split(",");
+                list.add(new Employee(fields[0], fields[1], Double.parseDouble(fields[2])));
+                line = br.readLine();
+            }
+
+            System.out.print("Enter salary: ");
+            double salary = sc.nextDouble();
+
+            List<String> emails = list.stream()
+                    .filter(employee -> employee.getSalary() > salary)
+                    .map(employee -> employee.getEmail())
+                    .sorted()
+                    .collect(Collectors.toList());
+
+            System.out.println("Email of people whose salary is more than " + String.format("%.2f", salary) + ": ");
+            emails.forEach(System.out::println);
+
+            double sum = list.stream()
+                    .filter(employee -> employee.getName().charAt(0) == 'M')
+                    .map(employee -> employee.getSalary())
+                    .reduce(0.0, Double::sum);
+
+            System.out.println("Sum of salary from people whose name starts with 'M': " + String.format("%.2f", sum));
+
+        } catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+        sc.close();
+    }
+}
