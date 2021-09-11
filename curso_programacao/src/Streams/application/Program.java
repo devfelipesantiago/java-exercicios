8ee2101374a613bbd95eeb46b21291b14d6e2b31
felipe <felipesantiagolds@gmail.com>
2021-09-11 16:23:13 -0300

    Pipeline

--- curso_programacao/src/Streams/application/Program.java
+++ curso_programacao/src/Streams/application/Program.java
@@ -2,13 +2,14 @@ package Streams.application;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Program {
     public static void main(String[] args) {
 
         // Criar Stream apartir da lista
-        List<Integer> list = Arrays.asList(3, 4, 5, 6, 7, 8);
+        List<Integer> list = Arrays.asList(3, 4, 5, 10, 7);
 
         Stream<Integer> st1 = list.stream();
         System.out.println(Arrays.toString(st1.toArray()));
@@ -29,6 +30,12 @@ public class Program {
                 .iterate(new Long[]{0L, 1L}, p -> new Long[]{p[1], p[0] + p[1]}).map(p -> p[0]);
         System.out.println(Arrays.toString(st5.limit(20).toArray()));
 
+        int sum = list.stream().reduce(0, Integer::sum);
+        System.out.println("Sum = " + sum);
+
+        System.out.println(Arrays.toString(list.stream()
+                .filter(x -> x % 2 == 0)
+                .map(x -> x * 10).toArray()));
 
     }
 }
