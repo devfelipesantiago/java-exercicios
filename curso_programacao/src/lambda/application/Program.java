d1cfd1da19901ac3e3eebf8da819797e39870cde
felipe <felipesantiagolds@gmail.com>
2021-09-11 12:44:00 -0300

    Map exercicios

---
+++ curso_programacao/src/lambda/application/Program.java
@@ -0,0 +1,33 @@
+package lambda.application;
+
+import lambda.entities.Product;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class Program {
+    public static void main(String[] args) {
+
+        List<Product> list = new ArrayList<>();
+
+        Comparator<Product> comp = new Comparator<Product>() {
+            @Override
+            public int compare(Product p1, Product p2) {
+                return p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
+            }
+        };
+
+        list.add(new Product("Tv", 900.0));
+        list.add(new Product("Notebook", 1200.0));
+        list.add(new Product("Tablet", 400.0));
+        /* Maneira ruim de comparar
+        list.sort(new myComparetor( ));*/
+
+        list.sort(comp);
+
+        for (Product p : list) {
+            System.out.println(p);
+        }
+    }
+}
