af1c3c50e3dd381a2e3e3f3b2542fd07361ce733
felipe <felipesantiagolds@gmail.com>
2021-09-11 14:36:22 -0300

    Function com expressao inline

---
+++ curso_programacao/src/function/application/Program.java
@@ -0,0 +1,25 @@
+package function.application;
+
+import function.entities.Product;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class Program {
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        List<Product> list = new ArrayList<>();
+
+        list.add(new Product("Tv", 900.0));
+        list.add(new Product("Notebook", 1200.0));
+        list.add(new Product("Tablet", 400.0));
+
+        List<String> name = list.stream()
+                .map(product -> product.getName().toUpperCase()).collect(Collectors.toList());
+        name.forEach(System.out::println);
+    }
+}
