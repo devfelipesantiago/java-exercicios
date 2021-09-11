022236192bb0250366df22eec4744ddde7f19c6e
felipe <felipesantiagolds@gmail.com>
2021-09-11 14:02:29 -0300

    Predicate

---
+++ curso_programacao/src/lambda/predicate/ProductPredicate.java
@@ -0,0 +1,13 @@
+package lambda.predicate;
+
+import lambda.entities.Product;
+
+import java.util.function.Predicate;
+
+public class ProductPredicate implements Predicate<Product> {
+
+    @Override
+    public boolean test(Product product) {
+        return product.getPrice() >= 100.0;
+    }
+}
